c4e2b98863cfea37a4864b1dcdc049a1631afef7
Aristo-eng <ribeus.munezero@yahoo.com>
2022-02-13 17:44:24 +0200

    argument packing

--- Week 4/sum_of_squares_arbitrary.py
+++ Week 4/sum_of_squares_arbitrary.py
@@ -9,3 +9,13 @@ def sum_of_squares(*t):
 print(sum_of_squares.__doc__)
 print(sum_of_squares(-2))
 print(sum_of_squares(-2,4,5))
+
+
+'''
+The strange looking argument notation (the star) is called argument packing.
+It packs all the given positional arguments into a tuple t.
+We will encounter tuples again later, but it suffices now to say that tuples are immutable lists. 
+With the for loop we can iterate through all the elements in the tuple.
+
+
+'''
