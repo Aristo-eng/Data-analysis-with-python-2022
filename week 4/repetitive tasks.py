0fdc485619e78b898a8b5feac5ca0b12ad53c9d7
Aristo-eng <ribeus.munezero@yahoo.com>
2022-02-13 16:12:36 +0200

    loops

---
+++ week 4/repetitive tasks.py
@@ -0,0 +1,10 @@
+i=1
+while i<1000:
+    print("Square of",i, "is", i*i)
+    i += 1
+print("Finished printing all the squares below 1000.")
+
+sum = 0
+for s in range(0 , 10):
+    sum = sum+ s
+print("Sum of #s from 0 to 9 is {}".format(sum))
