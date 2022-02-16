61c3fff444dcdfe718901aee64e377347e4f994d
Aristo-eng <ribeus.munezero@yahoo.com>
2022-02-16 09:57:37 +0200

    exercise 7 solution

--- exercise_7_areas of shapes/part01-e07_areas_of_shapes/src/areas_of_shapes.py
+++ exercise_7_areas of shapes/part01-e07_areas_of_shapes/src/areas_of_shapes.py
@@ -1,5 +1,39 @@
 #!/usr/bin/env python3
 
+# Exercise 7 (areas of shapes)
+# Create a program that can compute the areas of three shapes,
+# triangles, rectangles and circles, when their dimensions are given.
+#
+# An endless loop should ask for which shape you want the area be
+# calculated. An empty string as input will exit the loop. If the user
+# gives a string that is none of the given shapes, the message “unknown
+# shape!” should be printed. Then it will ask for dimensions for that
+# particular shape. When all the necessary dimensions are given, it
+# prints the area, and starts the loop all over again. Use format
+# specifier f for the area.
+#
+# What happens if you give incorrect dimensions, like giving string “aa”
+# as radius? You don’t have to check for errors in the input.
+#
+# Example interaction:
+#
+# Choose a shape (triangle, rectangle, circle): triangle
+# Give base of the triangle: 20
+# Give height of the triangle: 5
+# The area is 50.000000
+# Choose a shape (triangle, rectangle, circle): rectangel
+# Unknown shape!
+# Choose a shape (triangle, rectangle, circle): rectangle
+# Give width of the rectangle: 20
+# Give height of the rectangle: 4
+# The area is 80.000000
+# Choose a shape (triangle, rectangle, circle): circle
+# Give radius of the circle: 10
+# The area is 314.159265
+# Choose a shape (triangle, rectangle, circle):
+
+
+
 from cmath import pi
 import math
 
